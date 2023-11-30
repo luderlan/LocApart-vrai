3a6a421df2d499fa7beba980c2fcaef795ca9b53
luderlan <147407861+luderlan@users.noreply.github.com>
2023-11-30 16:36:51 +0100

    Update trait.client.php

--- assets/php/traitement/trait.client.php
+++ assets/php/traitement/trait.client.php
@@ -9,15 +9,14 @@ if (isset($_POST['updateCli'])) {
     $nom_client = $_POST['nom_client'][$id_client];
     $prenom_client = $_POST['prenom_client'][$id_client];
     $rue_client = $_POST['rue_client'][$id_client];
-    $code_client = $_POST['code_client'][$id_client];
-    $vil_client = $_POST['vil_client'][$id_client];
+    $code_comm = $_POST['code_comm'][$id_client];
     $mail_client = $_POST['mail_client'][$id_client];
     $pass_client = $_POST['pass_client'][$id_client];
     $statut_client = $_POST['statut_client'][$id_client];
     $valid_client = $_POST['valid_client'][$id_client];
 
 
-    $oClient->updateClient($id_client, $nom_client,$prenom_client,$rue_client,$code_client,$vil_client,$mail_client,$pass_client,
+    $oClient->updateClient($id_client, $nom_client,$prenom_client,$rue_client,$code_comm,$mail_client,$pass_client,
     $statut_client,$valid_client);
     header("location:../affichage/aff.client.php");
 } elseif (isset($_POST['deleteCli'])) {
