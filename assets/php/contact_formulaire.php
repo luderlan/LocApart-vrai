7df36d806ca2724f6912e59b12d81ed025a81269
luderlan <147407861+luderlan@users.noreply.github.com>
2023-12-14 14:33:48 +0100

    test

--- assets/php/contact_formulaire.php
+++ assets/php/contact_formulaire.php
@@ -1,30 +1,15 @@
 <?php
-$serveur = "localhost";  
-$utilisateur = "root";
-$mot_de_passe = "";
-$base_de_donnees = "locapart";
 
-$connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
+require_once("include/connexion.inc.php");
+require_once("class/class.contact.php");
 
-if ($connexion->connect_error) {
-    die("Échec de la connexion à la base de données : " . $connexion->connect_error);
-}
+    $prenom = ($_POST["prenom"]);
+    $nom = ($_POST["nom"]);
+    $email = ($_POST["email"]);
+    $telephone = ($_POST["telephone"]);
+    $message = ($_POST["message"]);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $prenom = $connexion->real_escape_string($_POST["prenom"]);
-    $nom = $connexion->real_escape_string($_POST["nom"]);
-    $email = $connexion->real_escape_string($_POST["email"]);
-    $telephone = $connexion->real_escape_string($_POST["telephone"]);
-    $message = $connexion->real_escape_string($_POST["message"]);
-
-    $requete = "INSERT INTO contact (prenom, nom, email, telephone, message) VALUES ('$prenom', '$nom', '$email', '$telephone', '$message')";
-
-    if ($connexion->query($requete) === TRUE) {
-        echo "Enregistrement réussi !";
-    } else {
-        echo "Erreur lors de l'enregistrement : " . $connexion->error;
-    }
-
-    $connexion->close();
-}
+    $oContact = new Contact($con);
+    $oContact->insertContact($prenom,$nom,$email,$telephone,$message);
+    
 ?>
